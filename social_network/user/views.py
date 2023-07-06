b5418a8b38542fac6bcf48d6cefd0bd14c6eff31
Dima Sheptytskyi <channektoshka@gmail.com>
2023-07-07 00:50:22 +0300

    feat: set user rules in view

--- social_network/user/views.py
+++ social_network/user/views.py
@@ -1,4 +1,3 @@
-import requests
 from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
@@ -8,6 +7,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.models import update_last_login
 from .models import CustomUserModel
 from .serializers import UserSerializer, UserActivitySerializer
+from .exceptions import MaxNumberOfUserException
+from .services import is_free_place_for_new_user
+from bot.config import SocialNetworkConfigRules
 
 
 class RegisterView(CreateAPIView):
@@ -15,6 +17,10 @@ class RegisterView(CreateAPIView):
     serializer_class = UserSerializer
 
     def post(self, request, *args, **kwargs):
+        config = SocialNetworkConfigRules.get_config()
+        if not is_free_place_for_new_user(config.number_of_users):
+            raise MaxNumberOfUserException(f'You can not register, server limited '
+                                           f'to {config.number_of_users} users')
         user = CustomUserModel.objects.create_user(**request.data)
         data = self.get_serializer(instance=user).data
         return Response(data, status=status.HTTP_201_CREATED)
